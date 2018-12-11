703bb6426d9b3ac6162a0a9028385cccfa01020c
fisker <lionkay@gmail.com>
2018-12-11 18:21:47 +0800

    build: a simple test to pass test

---
+++ __tests__/api.js
@@ -0,0 +1,7 @@
+const PromiseFileReader = require('../')
+
+describe('test explored object', () => {
+  test('PromiseFileReader is function', () => {
+    expect(PromiseFileReader).toBeInstanceOf(Function)
+  })
+})
