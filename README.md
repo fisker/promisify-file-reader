20ec7ef2851c1d98c81d5e64cf57f87ee5753f4c
fisker <lionkay@gmail.com>
2018-12-12 10:45:06 +0800

    refactor: redesign api

--- README.md
+++ README.md
@@ -1,94 +1,40 @@
-# Promise File Reader
+# Promisify File Reader
 
-## ustage
+`PromisifyFileReader` is a `class` with methods `readAsArrayBuffer`, `readAsText`, `readAsDataURL`, `readAsBinaryString` just as `window.FileReader` does, but returns `Promise` resolve with `FileReader.result` or reject with `FileReader.error`.
+
+## Usage
+
+## Basic syntax
 
 ```js
-import PromiseFileReader, {
-  FileReader,
-  File,
-  fileReader,
+import PromisifyFileReader from 'promisify-file-reader'
+
+;(async file => {
+  const fileReader = new PromisifyFileReader()
+  try {
+    console.log(await fileReader.readAsText(file))
+  } catch (err) {
+    console.error(err)
+  }
+
+})(new File(['hello from pfr.txt'], 'pfr.txt', {type: 'text/plain'}))
+```
+
+## Static methods
+
+```js
+import {
+  readAsArrayBuffer,
+  readAsText,
+  readAsDataURL,
+  readAsBinaryString
 } from 'promisify-file-reader'
-```
 
-## Objects
-
-default export object is `PromiseFileReader`
-
-#### PromiseFileReader
-
-a `class` with methods `readAsArrayBuffer`, `readAsText`, `readAsDataURL`,
-`readAsBinaryString` just as `window.FileReader` does, but returns `Promise`, resolve
-with `FileReader.result` or reject with `FileReader.error`.
-
-also exports as `window.PromiseFileReader`
-
-```js
-// import
-import PromiseFileReader from 'promisify-file-reader'
-```
-
-```js
-// example
 ;(async file => {
-  var pfr = new PromiseFileReader()
-  console.log(await pfr.readAsText(file))
-})(new File(['hello from pfr.txt'], 'pfr.txt'))
+  try {
+    console.log(await readAsDataURL(file))
+  } catch (err) {
+    console.error(err)
+  }
+})(new File(['hello from pfr.txt'], 'pfr.txt', {type: 'text/plain'}))
 ```
-
-#### PromiseFileReader.FileReader
-
-a reference of PromiseFileReader
-
-```js
-// import
-import {FileReader as PromiseFileReader} from 'promisify-file-reader'
-```
-
-#### PromiseFileReader.File
-
-a `class` takes a `Blob` Object in constructor.
-this also has 4 method as `window.FileReader`, but without `readAs` prefix
-
-also exports as `window.PromiseFile`
-
-```js
-// import
-import {File as PromiseFile} from 'promisify-file-reader'
-```
-
-```js
-// example
-;(async file => {
-  console.log(await new PromiseFile(file).arrayBuffer())
-  console.log(await new PromiseFile(file).text())
-  console.log(await new PromiseFile(file).dataURL())
-  console.log(await new PromiseFile(file).binaryString())
-})(new File(['hello from pfr.txt'], 'pfr.txt'))
-```
-
-#### PromiseFileReader.fileReader
-
-a instance of `PromiseFileReader`.
-unlike `window.FileReader`, `PromiseFileReader` don't need to listen `onload` or `onerror` event,
-so the reader can be re-use easily
-
-also exports as `window.promiseFileReader`
-
-```js
-// import
-import {fileReader as promiseFileReader} from 'promisify-file-reader'
-```
-
-```js
-// example
-;(async file => {
-  console.log(await promiseFileReader.readAsArrayBuffer(file))
-  console.log(await promiseFileReader.readAsText(file))
-  console.log(await promiseFileReader.readAsDataURL(file))
-  console.log(await promiseFileReader.readAsBinaryString(file))
-})(new File(['hello from pfr.txt'], 'pfr.txt'))
-```
-
-notice:
-if you call more then one read method, at the same time.
-don't use `promiseFileReader.result` and `promiseFileReader.error`
--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@
 
   var root = Function('return this')() // eslint-disable-line
   var FileReader = root.FileReader
-  var toString = Object.prototype.toString
-  var concat = Array.prototype.concat
   var dataTypes = [
     'ArrayBuffer',
     'Text',
@@ -13,99 +11,61 @@
     'BinaryString'
   ]
 
-  function noop () {}
-
-  function checkBlob (x) {
-    var type = toString.call(x).slice(8, -1)
-
-    if (type !== 'Blob' && type !== 'File') {
-      throw new TypeError(
-        'parameter 1 is not of type "Blob"'
-      )
+  function umdExport (name, mod) {
+    if (typeof define === 'function' && define.amd) {
+      define(name, [], function () {
+        return mod
+      })
+    } else if (typeof module === 'object' && module.exports) {
+      module.exports = mod
+    } else {
+      root[name] = mod
     }
+
+    return mod
   }
 
-  function readAsPromise (method, args, onLoad, onError) {
-    onLoad = onLoad || noop
-    onError = onError || noop
+  function readAsPromise (method, file, args) {
     var fileReader = new FileReader()
 
     return new Promise(function (resolve, reject) {
       fileReader.onload = function () {
-        var result = fileReader.result
-        resolve(result)
-        onLoad(result)
+        resolve(fileReader.result)
       }
 
       fileReader.onerror = function () {
-        var error = fileReader.error
-        resolve(error)
-        onError(error)
+        reject(fileReader.error)
       }
 
-      method.apply(fileReader, args)
+      if (args.length <= 1) {
+        method.call(fileReader, file)
+      } else {
+        method.apply(fileReader, args)
+      }
     })
   }
 
-  function callMethod (method) {
-    return function readFile () {
-      var reader = this
-      reader.result = null
-      reader.error = null
+  function callMethod (methodName) {
+    var method = FileReader.prototype[methodName]
 
+    return function readAs (file) {
       return readAsPromise(
         method,
-        arguments,
-        function (result) {
-          reader.result = result
-        },
-        function (error) {
-          reader.error = error
-        }
+        file,
+        arguments
       )
     }
   }
 
-  function callMethodWithFile (method) {
-    return function read () {
-      return readAsPromise(
-        method,
-        concat.call([this.file], arguments)
-      )
-    }
-  }
-
-  function PromiseFileReader () {}
-
-  function PromiseFile (blob) {
-    checkBlob(blob)
-    this.file = blob
-  }
+  function PromisifyFileReader () {}
 
   dataTypes.forEach(function (type) {
-    var readerMethod = 'readAs' + type
-    var fileMethod = type[0].toLowerCase() + type.slice(1)
-    var method = FileReader.prototype[readerMethod]
-    PromiseFileReader.prototype[readerMethod] = callMethod(method)
-    PromiseFile.prototype[fileMethod] = callMethodWithFile(method)
+    var methodName = 'readAs' + type
+    PromisifyFileReader.prototype[methodName] = callMethod(methodName)
+    PromisifyFileReader[methodName] = callMethod(methodName)
   })
 
+  PromisifyFileReader.reader = new PromisifyFileReader()
 
-  // explore
-  var promiseFileReader = new PromiseFileReader()
-  PromiseFileReader.FileReader = PromiseFileReader
-  PromiseFileReader.fileReader = promiseFileReader
-  PromiseFileReader.File = PromiseFile
-
-  if (typeof define === 'function' && define.amd) {
-    define('PromiseFile', [], function () {
-      return PromiseFileReader
-    })
-  } else if (typeof module === 'object' && module.exports) {
-    module.exports = PromiseFileReader
-  } else {
-    root.PromiseFile = PromiseFile
-    root.PromiseFileReader = PromiseFileReader
-    root.promiseFileReader = promiseFileReader
-  }
+  return umdExport('PromisifyFileReader', PromisifyFileReader)
 })()
