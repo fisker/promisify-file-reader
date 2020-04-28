1155b71c1e24b06e33c038c15a97691193d0ed98
fisker <lionkay@gmail.com>
2020-04-28 11:22:17 +0800

    fix: don't `Blob.text()`

--- src/core/text.js
+++ src/core/text.js
@@ -1,6 +1,6 @@
 import promisify from '../helper/promisify'
-import {Blob, FileReader} from '../helper/global-this'
+import {FileReader} from '../helper/global-this'
 
-const text = Blob.prototype.text || promisify(FileReader.prototype.readAsText)
+const text = promisify(FileReader.prototype.readAsText)
 
 export {text, text as readAsText}
